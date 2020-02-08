0289663501c85187e7cee4e8fc9c8b967cfce862
Grayce <grayce.ferreira@gmail.com>
2020-02-08 17:09:08 -0300

    Novos campos no UsuariosSchema

--- models/UsuariosSchema.js
+++ models/UsuariosSchema.js
@@ -7,6 +7,14 @@ const UsuariosSchema = new Schema({
   foto: { type: String, required: false },
   cpf: { type: String, require: true},
   senha: { type: String, required: true },
+  agencia: { type: String, required: true },
+  conta: { type: String, required: true },
+  telefone1: { type: String, required: true},
+  telefone2: {type: String, required: false},
+  endereco: { type: String, required: true },
+  bairro: { type: String, required: true },
+  cidade: { type: String, required: true },
+  estado: { type: String, required: true }
 })
 
 const usuariosModel = mongoose.model('usuarios',UsuariosSchema);
