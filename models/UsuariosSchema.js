9f98b40e71659a2dd48ee385c5b51f6cdeac7e35
Grayce Ferreira <grayce.ferreira@gmail.com>
2020-02-12 21:43:59 -0300

    Alteração Schema do Usuário

--- models/UsuariosSchema.js
+++ models/UsuariosSchema.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
+
 const UsuariosSchema = new Schema({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true, required: true },
   nome: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   foto: { type: String, required: false },
-  cpf: { type: String, require: true},
+  cpf: { type: String, require: true, unique: true},
   senha: { type: String, required: true },
-  agencia: { type: String, required: true },
-  conta: { type: String, required: true },
   telefone1: { type: String, required: true},
   telefone2: {type: String, required: false},
   endereco: { type: String, required: true },
+  numero: { type: Number, required: true },
   bairro: { type: String, required: true },
   cidade: { type: String, required: true },
   estado: { type: String, required: true }
