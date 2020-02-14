a1b4b7abf4673e7aafce0e06b1fb3cd82ef058f2
Grayce Ferreira <grayce.ferreira@gmail.com>
2020-02-14 01:04:24 -0300

    Extrato + controllers de transações

--- src/routes/extrato.js
+++ src/routes/extrato.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const controller = require("../controllers/TransferenciasController");
 
 
+<<<<<<< Updated upstream
 router.get('/extrato/detalhes/:transferenciaId', controller.getById)
 router.get('/extrato/:usuarioId', controller.getTransferenciasUsuario)
+=======
+router.get('/detalhes/:transferenciaId', controller.getById)
+router.get('/:contaOrigemId', controller.getTransferenciasConta)
+>>>>>>> Stashed changes
 
 
 module.exports = router
