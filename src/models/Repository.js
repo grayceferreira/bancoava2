da99dc52a4aaaadbc1cb4da84f10a0386e9b428c
Grayce Ferreira <grayce.ferreira@gmail.com>
2020-02-14 14:45:08 -0300

    Revert "Develop"

--- src/models/Repository.js
+++ src/models/Repository.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGO_URL = 'mongodb+srv://admin:Cepset1@@cluster0-vo2a0.mongodb.net/test?retryWrites=true&w=majority';
+const MONGO_URL = 'mongodb+srv://admin:admin@cluster0-3tdqy.mongodb.net/test?retryWrites=true&w=majority';
 
 function connect () {
   mongoose.connect(MONGO_URL,
